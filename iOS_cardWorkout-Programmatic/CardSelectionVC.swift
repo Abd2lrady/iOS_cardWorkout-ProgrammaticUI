e3f3ec527c6b3b0c041d3ab5795659d5034e407a
Abdelrady <abdelrady@Abdelradys-Mac.local>
2020-10-31 14:06:54 +0200

    starting app programmatically without storyboard

---
+++ iOS_cardWorkout-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,30 @@
+//
+//  CardSelectionVC.swift
+//  iOS_cardWorkout-Programmatic
+//
+//  Created by Abdelrady on 10/31/20.
+//  Copyright © 2020 Abdelrady. All rights reserved.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
